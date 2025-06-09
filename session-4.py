f104fb34d850906cb5a7ec2fad1139fa429da8ed
tasneemsherif <tasneemsherif45@gmail.com>
2025-06-09 19:38:52 +0300

    session-4

---
+++ session-4.py
@@ -0,0 +1,49 @@
+# Arithmetic oprators
+a = 20
+b = 10
+#print("-", a - b)
+#print("sum", a + b)
+#print(" * ", a * b)
+#print(" / ", a // b)
+#print("%", a % b)
+#print (" ** ", a ** b)
+
+
+#a *= 1  # a = a * 1
+#
+#a = 20 # assign
+#
+#a == 20 # is equal output True-false (bool)
+#
+#a <= 20 # not equal 
+
+# and 2 input = true 
+# or  false : 2 inputs = false-0
+# not ~ 1 -> 0 , # 0 -> 1
+#print (a in [1,2,3,4,5])
+
+#msg = [1, 5, 3, 4, 2]
+#print (5 in msg)
+
+#if (a == 20 and b == 10):
+#    print ("yes")
+#    if (a == 20 or b == 10):
+#        print ("yes")
+#        if (not a == 20):
+#            print ("yes")
+#            if (a in [1, 2, 3, 4, 5]):
+#                print ("yes")
+#                if (a not in [1, 2, 3, 4, 5]):
+#                    print ("yes")
+#
+#print (a is b) 
+
+if (a == 10):
+    print ("yes")
+    print ("yes")
+    a += 1
+    print (a) 
+elif (a == 20):
+    print ("maybe")
+else:
+    print ("no")
